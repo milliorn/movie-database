1188180b8b9463d3a5c2de4a01c8dd9a6935669f
milliorn <scottmilliorn@gmail.com>
2023-12-27 23:16:23 -0800

    Reorder imports in Header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
 
-import { Wrapper, Content, LogoImg, TMDBLogoImg } from "./Header.styles";
+import { Content, LogoImg, TMDBLogoImg, Wrapper } from "./Header.styles";
 
 const Header: React.FC = () => (
   <Wrapper>
