7dfff8919e7ba6ecacc11bf22e81a8669850107c
milliorn <scottmilliorn@gmail.com>
2024-01-29 16:30:47 -0800

    Refactor MovieInfo component

--- src/components/MovieInfo/index.tsx
+++ src/components/MovieInfo/index.tsx
@@ -15,52 +15,50 @@ type Props = {
 };
 
 const MovieInfo: React.FC<Props> = ({ movie }) => (
-  console.log(movie),
-  (
-    <Wrapper backdrop={movie.backdrop_path}>
-      <Content>
-        <Thumb
-          image={
-            movie.poster_path
-              ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
-              : NoImage
-          }
-          clickable={false}
-        />
-        <Text>
-          <h1>{movie.title}</h1>
-          <h2>"{movie.tagline}"</h2>
-          {/* <h3>PLOT</h3> */}
-          <p>{movie.overview}</p>
-          <div className="website">
-            <a target="_blank" href={movie.homepage} rel="noreferrer">
-              Click to the official website
-            </a>
+  // console.log(movie),
+  <Wrapper backdrop={movie.backdrop_path}>
+    <Content>
+      <Thumb
+        image={
+          movie.poster_path
+            ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}`
+            : NoImage
+        }
+        clickable={false}
+      />
+      <Text>
+        <h1>{movie.title}</h1>
+        <h2>"{movie.tagline}"</h2>
+        {/* <h3>PLOT</h3> */}
+        <p>{movie.overview}</p>
+        <div className="website">
+          <a target="_blank" href={movie.homepage} rel="noreferrer">
+            Click for official website
+          </a>
+        </div>
+        <div className="rating-directors">
+          <div className="rating">
+            <h3>RATING</h3>
+            <div className="score">{movie.vote_average.toPrecision(3)}</div>
           </div>
-          <div className="rating-directors">
-            <div className="rating">
-              <h3>RATING</h3>
-              <div className="score">{movie.vote_average.toPrecision(3)}</div>
-            </div>
-            <div className="director">
-              <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
-              {movie.directors.map((director) => (
-                <p key={director.credit_id}>{director.name}</p>
-              ))}
-            </div>
-            <div className="release">
-              <h3>RELEASE DATE</h3>
-              <p>{movie.release_date}</p>
-            </div>
-            <div className="status">
-              <h3>STATUS</h3>
-              <p>{movie.status}</p>
-            </div>
+          <div className="director">
+            <h3>DIRECTOR{movie.directors.length > 1 ? "S" : ""}</h3>
+            {movie.directors.map((director) => (
+              <p key={director.credit_id}>{director.name}</p>
+            ))}
           </div>
-        </Text>
-      </Content>
-    </Wrapper>
-  )
+          <div className="release">
+            <h3>RELEASE DATE</h3>
+            <p>{movie.release_date}</p>
+          </div>
+          <div className="status">
+            <h3>STATUS</h3>
+            <p>{movie.status}</p>
+          </div>
+        </div>
+      </Text>
+    </Content>
+  </Wrapper>
 );
 
 export default MovieInfo;
