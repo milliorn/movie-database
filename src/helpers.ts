3ffb52c3d797387b19248313aa02db0c4b6cca1b
milliorn <scottmilliorn@gmail.com>
2024-05-28 13:49:45 -0700

    feat: Refactor helper functions to use function declarations

--- src/helpers.ts
+++ src/helpers.ts
@@ -3,11 +3,11 @@
  * @param time - The time in minutes.
  * @returns A string representing the time in the format "Xh Ym".
  */
-const calcTime = (time: number): string => {
+function calcTime(time: number): string {
   const hours: number = Math.floor(time / 60);
   const mins: number = time % 60;
   return `${hours}h ${mins}m`;
-};
+}
 
 /**
  * Formatter for USD currency.
@@ -24,9 +24,9 @@ const usdFormatter = new Intl.NumberFormat("en-US", {
  * @param money - The amount of money to convert.
  * @returns The formatted string representing the money.
  */
-const convertMoney = (money: number): string => {
+function convertMoney(money: number): string {
   return usdFormatter.format(money);
-};
+}
 
 /**
  * Retrieves the persisted state from the session storage.
@@ -34,7 +34,7 @@ const convertMoney = (money: number): string => {
  * @param stateName - The name of the state to retrieve.
  * @returns The persisted state if found, otherwise null.
  */
-const getPersistedState = <T>(stateName: string): T | null | Error => {
+function getPersistedState<T>(stateName: string): T | null | Error {
   const sessionState = sessionStorage.getItem(stateName);
   try {
     return sessionState ? (JSON.parse(sessionState) as T) : null;
@@ -42,6 +42,6 @@ const getPersistedState = <T>(stateName: string): T | null | Error => {
     console.error("Failed to parse session state for key", stateName, ":", error);
     return null;
   }
-};
+}
 
 export { calcTime, convertMoney, getPersistedState };
