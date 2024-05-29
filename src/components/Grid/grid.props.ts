4245d23a5f34474f1c2dd13e209381cdf6808057
milliorn <scottmilliorn@gmail.com>
2024-05-29 13:30:49 -0700

    Refactor Grid component to use function declaration syntax and update import order

---
+++ src/components/Grid/grid.props.ts
@@ -0,0 +1,4 @@
+export type Props = {
+  header: string;
+  children: React.ReactNode;
+};
--- src/components/Grid/index.tsx
+++ src/components/Grid/index.tsx
@@ -1,15 +1,14 @@
 import React from "react";
-import { Wrapper, Content } from "./Grid.styles";
+import { Props } from "./grid.props";
+import { Content, Wrapper } from "./grid.styles";
 
-// types
-type Props = {
-  header: string;
-};
-const Grid: React.FC<Props> = ({ header, children }) => (
-  <Wrapper>
-    <h1>{header}</h1>
-    <Content>{children}</Content>
-  </Wrapper>
-);
+function Grid({ header, children }: Props) {
+  return (
+    <Wrapper>
+      <h1>{header}</h1>
+      <Content>{children}</Content>
+    </Wrapper>
+  );
+}
 
 export default Grid;
