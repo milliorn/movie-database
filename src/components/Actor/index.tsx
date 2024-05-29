31a0cdccb5af5308fb2014a4647b0020dfef98dc
milliorn <scottmilliorn@gmail.com>
2024-05-29 13:10:57 -0700

    Refactor Actor component to use function declaration syntax

--- src/components/Actor/index.tsx
+++ src/components/Actor/index.tsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Props } from "./actor.props";
 import { Image, Wrapper } from "./actor.styles";
 
-const Actor: React.FC<Props> = ({ name, character, imageUrl }) => (
-  <Wrapper>
-    <Image src={imageUrl} alt="actor-thumb" />
-    <h3>{name}</h3>
-    <p>{character}</p>
-  </Wrapper>
-);
+function Actor({ name, character, imageUrl }: Props): React.JSX.Element {
+  return (
+    <Wrapper>
+      <Image src={imageUrl} alt="actor-thumb" />
+      <h3>{name}</h3>
+      <p>{character}</p>
+    </Wrapper>
+  );
+}
 
 export default Actor;
