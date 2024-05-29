2634df38aaa03617a7cee1de416967bc1afede94
milliorn <scottmilliorn@gmail.com>
2024-05-29 13:32:16 -0700

    Refactor Header component to use function declaration syntax and update import order

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
 import RMDBLogo from "../../images/react-movie-logo.svg";
 import TMDBLogo from "../../images/tmdb_logo.svg";
+import { Content, LogoImg, TMDBLogoImg, Wrapper } from "./header.styles";
 
-import { Content, LogoImg, TMDBLogoImg, Wrapper } from "./Header.styles";
-
-const Header: React.FC = () => (
-  <Wrapper>
-    <Content>
-      <Link to="/">
-        <LogoImg src={RMDBLogo} alt="rmdb-logo" />
-      </Link>
-      <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
-    </Content>
-  </Wrapper>
-);
+function Header() {
+  return (
+    <Wrapper>
+      <Content>
+        <Link to="/">
+          <LogoImg src={RMDBLogo} alt="rmdb-logo" />
+        </Link>
+        <TMDBLogoImg src={TMDBLogo} alt="tmdb-logo" />
+      </Content>
+    </Wrapper>
+  );
+}
 
 export default Header;
