9ced2644ec134df25840f7515aa22d77d8d0c9df
milliorn <scottmilliorn@gmail.com>
2024-05-30 17:10:19 -0700

    refactor: update import path for Thumb component

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { ThumbProps } from "./thumb.props";
-import { Image, RatingText, ThumbInfoContainer } from "./thumb.styles";
+import { Image, RatingText, ThumbInfoContainer } from "./styles";
 
 function Thumb({
   image, movieId, clickable, rating, vote_count,
