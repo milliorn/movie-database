6e434cf2d728da5a5d0491b3282866e9bbbf6d9b
milliorn <scottmilliorn@gmail.com>
2024-06-03 16:26:21 -0700

    refactor: Update config.ts
    Refactor the config.ts file to improve readability and maintainability. This change includes adding JSDoc comments to document the purpose of each constant and enum, as well as organizing the code for better clarity.

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,11 @@
+/**
+ * The base URL for retrieving movie images from the TMDB API.
+ */
 const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/";
 
+/**
+ * Enum representing different image sizes available for movie images.
+ */
 enum ImageSizes {
   Small = "w300",
   Medium = "w780",
@@ -7,9 +13,19 @@ enum ImageSizes {
   Original = "original",
 }
 
+/**
+ * The size of the backdrop image to be used.
+ */
 const BACKDROP_SIZE = ImageSizes.Large;
+
+/**
+ * The size of the poster image to be used.
+ */
 const POSTER_SIZE = ImageSizes.Medium;
 
+/**
+ * The URL for the backend API.
+ */
 const BACKEND_API_URL = process.env.REACT_APP_BACKEND_API_URL;
 
 export { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE, BACKEND_API_URL };
