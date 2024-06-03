5e25e6e252e37950a59f5a6770bb92a407029bc6
milliorn <scottmilliorn@gmail.com>
2024-06-03 15:47:07 -0700

    feat: Update Button component props and add styled wrapper

--- src/components/Button/props.ts
+++ src/components/Button/props.ts
@@ -1,4 +1,14 @@
+/**
+ * Represents the props for the Button component.
+ */
 export type ButtonProps = {
+  /**
+   * The callback function to be executed when the button is clicked.
+   */
   callback: () => void;
+
+  /**
+   * The text to be displayed on the button.
+   */
   text: string;
 };
