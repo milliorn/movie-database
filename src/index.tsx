5f735d55c3d218604c06a0e81b5f7448d0113968
milliorn <scottmilliorn@gmail.com>
2024-06-06 00:59:12 -0700

    chore: Update React and ReactDOM imports, and use createRoot for rendering the App component

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,20 @@
  * Entry point of the application.
  * Renders the App component inside the root element of the HTML document.
  */
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+// import React from "react";
+// import ReactDOM from "react-dom";
+// import App from "./App";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root"),
-);
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById("root"),
+// );
+
+import { createRoot } from 'react-dom/client';
+import App from './App';
+
+const container = document.getElementById('root') ?? document.createElement('div');
+const root = createRoot(container); // createRoot(container!) if you use TypeScript
+root.render(<App />);
