5b28afef8b5ca2d736d399732e2b1aa1357ede1f
Scott Milliorn <scottmilliorn@gmail.com>
2024-06-15 23:32:56 -0700

    Now playing component, hook (#451)

---
+++ src/components/NowPlayingMovies/index.tsx
@@ -0,0 +1,45 @@
+import { BACKDROP_SIZE, IMAGE_BASE_URL, POSTER_SIZE } from "../../config";
+import useNowPlayingMovies from "../../hooks/useNowPlayingMovies";
+import Button from "../Button";
+import Grid from "../Grid";
+import HeroImage from "../HeroImage";
+import Spinner from "../Spinner";
+import Thumb from "../Thumb";
+import NoImage from "../../images/no_image.jpg";
+
+
+function NowPlayingMovies() {
+  const { state, loading, error, setIsLoadingMore } = useNowPlayingMovies();
+
+  if (error) return <div>Something went wrong...oops!</div>;
+
+  return (
+    <div>
+      {state.results[ 0 ] && (
+        <HeroImage
+          image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[ 0 ].backdrop_path}`}
+          title={state.results[ 0 ].original_title}
+          text={state.results[ 0 ].overview}
+        />
+      )}
+      <Grid header="Now Playing Movies">
+        {state.results.map(movie => (
+          <Thumb
+            key={movie.id}
+            clickable
+            image={movie.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${movie.poster_path}` : NoImage}
+            movieId={movie.id}
+            rating={movie.vote_average}
+            vote_count={movie.vote_count}
+          />
+        ))}
+      </Grid>
+      {loading && <Spinner />}
+      {state.page < state.total_pages && !loading && (
+        <Button text="Load More" callback={() => setIsLoadingMore(true)} />
+      )}
+    </div>
+  );
+}
+
+export default NowPlayingMovies;
---
+++ src/hooks/useNowPlayingMovies.ts
@@ -0,0 +1,47 @@
+import { useEffect, useState } from "react";
+import { api } from "../API";
+import { MoviePropTypes } from "../Global.props";
+
+const initialState = {
+  page: 0,
+  results: [] as MoviePropTypes[],
+  total_pages: 0,
+  total_results: 0,
+};
+
+function useNowPlayingMovies() {
+  const [ state, setState ] = useState(initialState);
+  const [ loading, setLoading ] = useState(false);
+  const [ error, setError ] = useState(false);
+  const [ isLoadingMore, setIsLoadingMore ] = useState(false);
+
+  const fetchMovies = async (page: number) => {
+    setLoading(true);
+    setError(false);
+    try {
+      const movies = await api.fetchNowPlayingMovies(page);
+      setState(prev => ({
+        ...movies,
+        results: page > 1 ? [ ...prev.results, ...movies.results ] : movies.results,
+      }));
+    } catch (err) {
+      setError(true);
+      console.error("Failed to fetch now playing movies:", err);
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchMovies(1);
+  }, []);
+
+  useEffect(() => {
+    if (!isLoadingMore) return;
+    fetchMovies(state.page + 1);
+    setIsLoadingMore(false);
+  }, [ isLoadingMore, state.page ]);
+
+  return { state, loading, error, setIsLoadingMore };
+}
+
+export default useNowPlayingMovies;
