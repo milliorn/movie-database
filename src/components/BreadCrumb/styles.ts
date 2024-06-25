2d019b025c216c8032215954cb7a15910dc360fb
milliorn <scottmilliorn@gmail.com>
2024-06-25 15:03:10 -0700

    style: Add pulse animation to button in BreadCrumb component

--- src/components/BreadCrumb/styles.ts
+++ src/components/BreadCrumb/styles.ts
@@ -30,4 +30,17 @@ export const Content = styled.div`
       font-size: var(--fontSmall);
     }
   }
+
+  button {
+    animation: pulse 2s cubic-bezier(0.4, 0, 0.6, 1) infinite;
+
+    @keyframes pulse {
+      0%, 100% {
+        opacity: 1;
+      }
+      50% {
+        opacity: .5;
+      }
+    }
+  }
 `;
