f9faedd33dfad651d7b23713eb505cc8220b9015
milliorn <scottmilliorn@gmail.com>
2024-06-25 16:28:39 -0700

    feat: Preload Largest Contentful Paint image with Helmet

--- src/components/HeroImage/index.tsx
+++ src/components/HeroImage/index.tsx
@@ -1,40 +1,31 @@
 import React, { useEffect, useState } from "react";
+import { Helmet } from "react-helmet";
 import { HeroImageProps } from "./props";
 import { Content, Text, Wrapper } from "./styles";
 
-/**
- * Renders a hero image component with a title and text.
- *
- * @param {HeroImageProps} props - The component props.
- * @param {string} props.image - The URL of the image to be displayed.
- * @param {string} props.title - The title to be displayed.
- * @param {string} props.text - The text to be displayed.
- * @returns {React.JSX.Element} The rendered hero image component.
- */
 function HeroImage({ image, title, text }: HeroImageProps): React.JSX.Element {
-  // State to track if the image has loaded
   const [loaded, setLoaded] = useState(false);
 
   useEffect(() => {
-    // Image preloading: Create a new Image object to manage loading
     const img = new Image();
-    // Set the source, initiating the loading process
     img.src = image;
-    // On image load completion, set 'loaded' to true to signify readiness
     img.onload = () => setLoaded(true);
-    // Re-run this effect if 'image' prop changes, allowing for reactive updates
   }, [image]);
 
   return (
-    <Wrapper $image={loaded ? image : ""}>
-      {/* Render the image or nothing based on its loaded state to prevent layout shifts */}
-      <Content>
-        <Text>
-          <h1>{title}</h1>
-          <p>{text}</p>
-        </Text>
-      </Content>
-    </Wrapper>
+    <>
+      <Helmet>
+        <link rel="preload" href={image} as="image" />
+      </Helmet>
+      <Wrapper $image={loaded ? image : ""}>
+        <Content>
+          <Text>
+            <h1>{title}</h1>
+            <p>{text}</p>
+          </Text>
+        </Content>
+      </Wrapper>
+    </>
   );
 }
 
