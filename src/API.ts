7ec4b669c43771af139e80df763a3ab1c191ec78
milliorn <scottmilliorn@gmail.com>
2024-07-12 15:31:51 -0700

    feat: Update API endpoint in API.ts file
    The API.ts file was updated to use a more readable and concise endpoint URL. This change aims to improve the maintainability and clarity of the codebase.

--- src/API.ts
+++ src/API.ts
@@ -70,10 +70,15 @@ const api = {
    * @param page - The page number of the movie list.
    * @returns A promise that resolves to a Movies object.
    */
-  fetchUpcomingMovies: async (page: number): Promise<Movies> => {
-    const response = await fetch(
-      `${BACKEND_API_URL}/api/movies/upcoming?page=${page}`,
-    );
+  fetchUpcomingMovies: async (
+    page: number,
+    searchTerm?: string,
+  ): Promise<Movies> => {
+    const endpoint = searchTerm
+      ? `${BACKEND_API_URL}/api/movies?searchTerm=${searchTerm}&page=${page}`
+      : `${BACKEND_API_URL}/api/movies/upcoming?page=${page}`;
+
+    const response = await fetch(endpoint);
     return await response.json();
   },
   /**
