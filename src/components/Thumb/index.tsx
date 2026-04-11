3e6759c2c9d30608ead10077dbb5b2df9220784c
Scott Milliorn <scottmilliorn@gmail.com>
2026-04-11 13:29:23 -0700

    Add width and height attributes to Image component in Thumb for better performance

--- src/components/Thumb/index.tsx
+++ src/components/Thumb/index.tsx
@@ -20,10 +20,10 @@ function Thumb({
     <div className="">
       {clickable ? (
         <Link to={`/${movieId}`}>
-          <Image src={image} alt="movie-thumb" $isClicked={true} />
+          <Image src={image} alt="movie-thumb" $isClicked={true} width={780} height={1170} />
         </Link>
       ) : (
-        <Image src={image} alt="movie-thumb" $isClicked={false} />
+        <Image src={image} alt="movie-thumb" $isClicked={false} width={780} height={1170} />
       )}
       <ThumbInfoContainer>
         <RatingText>Rating: {rating?.toFixed(1)}</RatingText>
