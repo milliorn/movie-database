f43ea99d543a59c10371c4a78ada0e4abf326382
Scott Milliorn <scottmilliorn@gmail.com>
2026-04-15 22:33:38 -0700

    refactor(Spinner): add return type annotation for Spinner component

--- src/components/Spinner/index.tsx
+++ src/components/Spinner/index.tsx
@@ -1,8 +1,9 @@
 /**
  * A spinner component that indicates loading or processing state.
  */
+import type { ReactElement } from "react";
 import { Spinner as SpinnerEl } from "./styles";
 
-export default function Spinner() {
+export default function Spinner(): ReactElement {
   return <SpinnerEl data-testid="spinner" />;
 }
